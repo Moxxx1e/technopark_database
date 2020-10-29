a9b1ccb074f9401a1961d137e7bd3c0cb400346c
Moxxx1e <moxxxie07@gmail.com>
2020-10-29 22:19:08 +0300

    Добавить модели для каждой сущности

---
+++ internal/models/post.go
@@ -0,0 +1,14 @@
+package models
+
+import "time"
+
+type Post struct {
+	Id       int       `json:"id"`
+	Parent   int       `json:"parent"`
+	Author   string    `json:"author"`
+	Message  string    `json:"message"`
+	IsEdited bool      `json:"isEdited"`
+	Forum    string    `json:"forum"`
+	Thread   int       `json:"thread"`
+	Created  time.Time `json:"created"`
+}
