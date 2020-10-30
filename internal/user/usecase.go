f29b33039a8800e25b1423d548e1878a646c4230
Moxxx1e <moxxxie07@gmail.com>
2020-10-30 14:37:04 +0300

    Переписать юз-кейсы для юзера с учётом кастомных ошибок

--- internal/user/usecase.go
+++ internal/user/usecase.go
@@ -1,9 +1,12 @@
 package user
 
-import "github.com/technopark_database/internal/models"
+import (
+	"github.com/technopark_database/internal/helpers/errors"
+	"github.com/technopark_database/internal/models"
+)
 
 type UserUseCase interface {
-	Create(user *models.User) error
-	UpdateUserInfo(nickname string, user *models.User) error
-	GetUserInfo(nickname string) (*models.User, error)
+	Create(user *models.User) *errors.Error
+	UpdateUserInfo(nickname string, user *models.User) *errors.Error
+	GetUserInfo(nickname string) (*models.User, *errors.Error)
 }
--- internal/user/usecases/user_usecase.go
+++ internal/user/usecases/user_usecase.go
@@ -2,6 +2,8 @@ package usecases
 
 import (
 	"database/sql"
+	"github.com/technopark_database/internal/consts"
+	"github.com/technopark_database/internal/helpers/errors"
 	"github.com/technopark_database/internal/models"
 	"github.com/technopark_database/internal/user"
 )
@@ -14,20 +16,28 @@ func NewUserUseCase(repository user.UserRepository) *UserUseCase {
 	return &UserUseCase{rep: repository}
 }
 
-func (uc *UserUseCase) Create(user *models.User) error {
-	return uc.rep.Insert(user)
+func (uc *UserUseCase) Create(user *models.User) *errors.Error {
+	err := uc.rep.Insert(user)
+	if err != nil {
+		return errors.New(consts.CodeInternalServerError, err)
+	}
+	return nil
 }
 
-func (uc *UserUseCase) UpdateUserInfo(nickname string, user *models.User) error {
-	return uc.rep.Update(nickname, user)
+func (uc *UserUseCase) UpdateUserInfo(nickname string, user *models.User) *errors.Error {
+	err := uc.rep.Update(nickname, user)
+	if err != nil {
+		return errors.New(consts.CodeInternalServerError, err)
+	}
+	return nil
 }
 
-func (uc *UserUseCase) GetUserInfo(nickname string) (*models.User, error) {
+func (uc *UserUseCase) GetUserInfo(nickname string) (*models.User, *errors.Error) {
 	info, err :=  uc.rep.Select(nickname)
 	if err == sql.ErrNoRows {
-		return nil, nil
+		return nil, errors.Get(consts.CodeUserDoesNotExist)
 	} else if err != nil {
-		return nil, err
+		return nil, errors.New(consts.CodeInternalServerError, err)
 	}
 	return info, nil
 }
