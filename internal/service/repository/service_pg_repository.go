2e0eb2886859c4896369fb5572a8d81101809ab5
Moxxx1e <moxxxie07@gmail.com>
2021-01-04 13:56:41 +0300

    Исправить подсчёт записей в базе

--- internal/service/repository/service_pg_repository.go
+++ internal/service/repository/service_pg_repository.go
@@ -13,40 +13,16 @@ type ServicePgRepository struct {
 
 func (rep ServicePgRepository) GetStatus() (*models.ServiceStatus, error) {
 	serviceStatus := &models.ServiceStatus{}
-	rows, err := rep.db.Query(`
-		SELECT reltuples::bigint AS estimate
-		FROM pg_class
-		WHERE relname='users' OR relname='forums' 
-			OR relname='threads' OR relname='posts'
-		ORDER BY relname`)
+	row := rep.db.QueryRow(`
+		SELECT
+		(SELECT count(*) from forums) AS forum,
+		(SELECT count(*) from posts) AS post,
+		(SELECT count(*) from threads) AS thread,
+		(SELECT count(*) from users) AS user`)
+
+	err := row.Scan(&serviceStatus.Forum, &serviceStatus.Post,
+		&serviceStatus.Thread, &serviceStatus.User)
 	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
-
-	for rows.Next() {
-		err = rows.Scan(&serviceStatus.Forum)
-		if err != nil {
-			return nil, err
-		}
-
-		err = rows.Scan(&serviceStatus.Post)
-		if err != nil {
-			return nil, err
-		}
-
-		err = rows.Scan(&serviceStatus.Thread)
-		if err != nil {
-			return nil, err
-		}
-
-		err = rows.Scan(&serviceStatus.User)
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	if rows.Err() != nil {
 		return nil, err
 	}
 
@@ -63,10 +39,8 @@ func (rep ServicePgRepository) Delete() error {
 		return err
 	}
 
-	_, err = tx.Exec(`TRUNCATE TABLE users CASCADE;
-				TRUNCATE TABLE forums CASCADE;
-				TRUNCATE TABLE posts CASCADE;
-				TRUNCATE TABLE threads CASCADE`)
+	_, err = tx.Exec(`
+		TRUNCATE users, forums, posts, threads, votes RESTART IDENTITY CASCADE`)
 	if err != nil {
 		_ = tx.Rollback()
 		return err
